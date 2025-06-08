f662ef57c33e92a436772d3c2a7161f55395b9a8
akkii1306 <akanksha.230101012@iiitbh.ac.in>
2025-06-08 10:05:32 +0530

    1st commit

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { Globe } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-left">
+        <h1 className="logo">KrishiHelp</h1>
+      </div>
+      <nav className="header-center">
+        <Link to="/">Home</Link>
+        <Link to="/tools">Tools</Link>
+        <Link to="/register">Register</Link>
+       
+      </nav>
+      <div className="header-right">
+        <Globe size={24} className="language-icon" title="Change Language" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,2 @@
+const Home = () => <h2>Welcome to KrishiHelp</h2>;
+export default Home;
